d0f3db0280000187f825a8092d1279bd9486dafd
Jordan Buonforte <jordan@Jordans-MacBook-Air.local>
2018-05-03 15:08:51 -0600

    done

--- controller.js
+++ controller.js
@@ -1,11 +1,12 @@
-module.exports={
-    getPlanes: (res, req, next) =>{
-        req.app.get('db').get_planes([25]).then(planes=>{
-            res.status(200).send(planes)
-        }).catch(err=>{
-            console.logg(err)
-            res.sendStatus(500)
-        })
-    },
+module.exports = {
+    getPlanes(req, res, next){
+        req.app.get('db')
+            .get_planes([25])
+            .then(planes => res.send(planes))
+            .catch(err => {
+                console.log(err)
+                res.status(500).send(err)
+            })
+    }
 
 }
